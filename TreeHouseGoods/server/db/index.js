3b3aafc0d44206a84a2f2143bdc5a704fab9ca8f
an emard <anemard00@gmail.com>
2022-11-08 16:27:37 -0600

    created db structure, created seed file, added apis

--- TreeHouseGoods/server/db/index.js
+++ TreeHouseGoods/server/db/index.js
@@ -3,12 +3,23 @@
 const db = require('./db')
 
 const User = require('./models/User')
+const Product = require('./models/Product')
+const Order = require('./models/Order')
+const Order_product = require('./models/Order_product')
 
-//associations could go here!
+
+Order.belongsTo(User);
+User.hasMany(Order);
+
+Order.belongsToMany(Product, {through: "Order_product"})
+Product.belongsToMany(Order, {through: "Order_product"})
 
 module.exports = {
   db,
   models: {
     User,
+    Product,
+    Order,
+    Order_product
   },
 }
