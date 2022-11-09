b51127df30fb802c63c0e1a9c191d9143fbe3f1e
an emard <anemard00@gmail.com>
2022-11-09 15:14:11 -0600

    finished single product slice

---
+++ TreeHouseGoods/client/app/SingleProductSlice/SingleProductSlice.js
@@ -0,0 +1,26 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
+
+const initialState = {
+    product: {},
+    loading: false,
+}
+export const fetchSingleProduct = createAsyncThunk('fetchSingleProduct', async (id) => {
+    const { data } = await axios.get(`/api/products/${id}`)
+    return data
+})
+const singleProductSlice = createSlice({
+    name: 'product',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchSingleProduct.pending, (state, action) => {
+            state.loading = true
+        }),
+        builder.addCase(fetchSingleProduct.fulfilled, (state, action) => {
+            state.loading = false
+            state.product = action.payload
+        })
+    }
+})
+export default singleProductSlice.reducer
--- TreeHouseGoods/client/features/products/SingleProduct.js
+++ TreeHouseGoods/client/features/products/SingleProduct.js
@@ -1,6 +1,15 @@
 import React from "react";
 
 const SingleProduct = () => {
+    const { id } = useParams();
+    const product = useSelector((state) => state.product.product)
+    
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(fetchsingleProduct())
+    }, [])
+    console.log(dispatch)
+
     return (
         <h1>Single Product</h1>
     )
