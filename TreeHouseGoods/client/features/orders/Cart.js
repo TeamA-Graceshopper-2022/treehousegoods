964702878edc1ddba7078d5f7a125927b80727c3
Erinnegr1 <erinne.grant@gmail.com>
2022-11-14 09:43:15 -0500

    cart navigation route UI change

--- TreeHouseGoods/client/features/orders/Cart.js
+++ TreeHouseGoods/client/features/orders/Cart.js
@@ -13,29 +13,10 @@ const Cart = () => {
         <div className="cart-empty">
           <p>Your cart is currently empty</p>
           <div className="start-shopping">
-          <span>Start Shopping</span>
-            <Link to="/">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                fill="currentColor"
-                className="bi bi-arrow-right"
-                viewBox="0 0 16 16"
-              >
-
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
-</svg>
-                <path
-                  fillRule="evenodd"
-                  d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
-                  />
-              </svg>
-              
-            </Link>
+            <Link to="/">Start Shopping </Link>
+            </div>
           </div>
-        </div>
+        
       ) : (
        <CartSummary />
       )}
