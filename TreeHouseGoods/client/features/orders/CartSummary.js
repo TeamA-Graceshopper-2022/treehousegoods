de1960907c80108bcf66f1ddf0a55ef135422e83
Erinnegr1 <erinne.grant@gmail.com>
2022-11-14 08:53:51 -0500

    add to guest cart function working. Display in cart in progress

--- TreeHouseGoods/client/features/orders/CartSummary.js
+++ TreeHouseGoods/client/features/orders/CartSummary.js
@@ -1,12 +1,29 @@
 import React from "react";
 import { Checkout } from "../"
 
+
 const CartSummary = () => {
     return(
         <div>
-        <h1>Cart Summary</h1>
-        <Checkout/>
+            <div>
+             <h1>Cart Summary</h1>
+             <h3>Product</h3>
+             <h3>Price</h3>
+             <h3>Quantity</h3>
+             <h3>Total</h3>
         </div>
+       {/* <div className="cart-items">
+            {Cart.cartItems &&
+            cart.cartItems.map((cartItem)=> (
+            <div className="cart-item" key={cartItem.id}>
+               <h3>{cartItem.name}</h3>
+             </div>
+            )
+        )}
+            </div> */}
+
+        <Checkout/>
+       </div>
     )
 }
 
