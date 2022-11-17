cced566ba1ee2b9c2d28df62294199f2856aac44
Erinnegr1 <erinne.grant@gmail.com>
2022-11-17 08:31:34 -0500

    styling update

--- TreeHouseGoods/client/features/home/LandingPage.js
+++ TreeHouseGoods/client/features/home/LandingPage.js
@@ -4,21 +4,21 @@ import { Link } from "react-router-dom";
 const LandingPage = () => {
   return (
     <div>
+        {/**
       <div className="landingImageBlock">
         <img
           id="landingPageImage"
           src="https://cdn.shopify.com/s/files/1/0553/0461/8173/products/junzo-red-clay-tokoname-japanese-teapot-set-88oz260ml-sasame-and-ceramesh-musubi-kiln-handmade-japanese-tableware-and-japanese-dinnerware-836048_1000x.jpg?v=1654483376"
         />
       </div>
-{/** 
+ */}
       <section id="carousel">
       <div id="carousel-text">
         <h1>Welcome to TreeHouse Goods</h1>
         <h2>Shop Button?</h2>
       </div>
       <img class="carousel-image" src="https://www.farmersalmanac.com/wp-content/uploads/2017/02/Breakfast-In-Bed-Coffee-Flowers-as_283245010.jpeg" />
-      <img class="carousel-image" src="https://cdn.shopify.com/s/files/1/0553/0461/8173/products/junzo-red-clay-tokoname-japanese-teapot-set-88oz260ml-sasame-and-ceramesh-musubi-kiln-handmade-japanese-tableware-and-japanese-dinnerware-836048_1000x.jpg?v=1654483376" />
-    </section>*/}
+    </section>
       
 
         <div className="categoryButtons">
